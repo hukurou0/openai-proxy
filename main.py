a73d433a4677e5a87425d6d7a10bd60ac9885ad2
hukurou <junjun.0712.on@gmail.com>
2023-12-16 01:38:49 +0900

    各ユーザごとにusageファイルを作成

--- main.py
+++ main.py
@@ -24,10 +24,15 @@ def get_api_key_by_user_id(in_user_id):
         if user_id == in_user_id:
             return api_key
 
-def init_user(request):
+def init():
     #log.txtとusage.txtを初期化する
     GCS.write_to_storage(BUCKET_NAME, "log.txt", "")
-    GCS.write_to_storage(BUCKET_NAME, "usage.txt", "Total Prompt Tokens:0\nTotal Completion Tokens:0\n")
+    users = GCS.read_from_storage(BUCKET_NAME, "users.txt")
+    users_list = users.split("\n")
+    del users_list[0]
+    for line in users_list:
+        name, user_id, api_key = line.split(":")
+        GCS.write_to_storage(BUCKET_NAME, f"{name}_usage.txt", "Total Prompt Tokens:0\nTotal Completion Tokens:0\n")
 
 def log(request):
     pass
@@ -37,7 +42,7 @@ def main(request):
     # Track token usage and any other required logic
     auth_header = request.headers.get("Authorization")
     user_id = auth_header.split(" ")[-1] if auth_header else None
-     
+    
     if user_id:
         api_key = get_api_key_by_user_id(user_id)
         api_key = api_key[:-1] 
@@ -56,16 +61,16 @@ def main(request):
 
             user_name = get_name_by_user_id(user_id)
             log_entry = f"{time.strftime('%Y-%m-%d %H:%M:%S')} | User: {user_name} | Prompt Tokens: {prompt_tokens} | Completion Tokens: {completion_tokens}\n"
-
             GCS.add_text_to_file(BUCKET_NAME, "log.txt", log_entry)
-            usage = GCS.read_from_storage(BUCKET_NAME, "usage.txt")
+            file_name = f"{user_name}_usage.txt"
+            usage = GCS.read_from_storage(BUCKET_NAME, file_name)
             lines = usage.split("\n")
             total_prompt_tokens = int(lines[0].split(":")[1])
             total_completion_tokens = int(lines[1].split(":")[1])
             total_prompt_tokens += prompt_tokens
             total_completion_tokens += completion_tokens
             usage = f"Total Prompt Tokens:{total_prompt_tokens}\nTotal Completion Tokens:{total_completion_tokens}\n"
-            GCS.write_to_storage(BUCKET_NAME, "usage.txt", usage)    
+            GCS.write_to_storage(BUCKET_NAME, file_name, usage)    
             
             # Return the response from OpenAI's API
             return response.json()

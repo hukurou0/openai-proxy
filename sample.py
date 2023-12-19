b7addb93ed88249f2e04c043486d6fa60bf86534
hukurou <junjun.0712.on@gmail.com>
2023-12-19 21:31:07 +0900

    update sample

--- sample.py
+++ sample.py
@@ -1,8 +1,18 @@
 import requests
 
-headers = {
-        'Authorization': 'Bearer $<user_id>',
-    }
-response = requests.post("https://asia-northeast2-kgavengers.cloudfunctions.net/openai-proxy/v1/chat/completions",headers=headers, json={"model": "gpt-4","messages": [{"role": "user", "content": "Hello world!"}]})
 
-print(response.text)
+#ここを編集#
+user_id = ""
+host = "https://asia-northeast2-kgavengers.cloudfunctions.net/openai-proxy"
+path = "/v1/chat/completions"
+data = {"model": "gpt-4","messages": [{"role": "user", "content": "Hello world!"}]}
+###########
+
+
+headers = {
+        'Authorization': f'Bearer {user_id}',
+    }
+
+response = requests.post(host+path,headers=headers, json=data)
+
+print(response.json())

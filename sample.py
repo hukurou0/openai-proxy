79f1c39ee7551d2066778d5e0ce89310c15923c5
hukurou <junjun.0712.on@gmail.com>
2023-12-19 21:18:25 +0900

    fix sample.py

--- sample.py
+++ sample.py
@@ -1,7 +1,7 @@
 import requests
 
 headers = {
-        'Authorization': 'Bearer envepskcje',
+        'Authorization': 'Bearer $<user_id>',
     }
 response = requests.post("https://asia-northeast2-kgavengers.cloudfunctions.net/openai-proxy/v1/chat/completions",headers=headers, json={"model": "gpt-4","messages": [{"role": "user", "content": "Hello world!"}]})
 
